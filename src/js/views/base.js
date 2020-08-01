08ede2827959c2577874f9ab8359aca512e4e999
sourav saha <souravsaha675@gmail.com>
2020-08-02 01:12:18 +0530

    all Dom element

---
+++ src/js/views/base.js
@@ -0,0 +1,31 @@
+export const elements = {
+    searchForm: document.querySelector(".search"),
+    searchInput: document.querySelector(".search__field"),
+    searchResultsList: document.querySelector(".results__list"),
+    searchResult : document.querySelector(".results"),
+    searchResultPage : document.querySelector(".results__pages") 
+}
+
+export const elementString = {
+    loader: "loader"
+};
+
+export const renderLoader = parent =>{
+    
+    const loader = `
+        <div class="${elementString.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div> 
+    `;
+
+    parent.insertAdjacentHTML("afterbegin", loader);
+};
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementString.loader}`);
+    if(loader){
+        loader.parentElement.removeChild(loader);
+    }
+}
