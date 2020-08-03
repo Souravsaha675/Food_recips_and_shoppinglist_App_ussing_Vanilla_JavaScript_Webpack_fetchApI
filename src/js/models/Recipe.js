cb57bf14dc6442289bd1a78fbb674df6ae5f0041
sourav saha <souravsaha675@gmail.com>
2020-08-04 00:11:26 +0530

    Undifiend & Nan displaying bug fixed

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -36,4 +36,74 @@ export default class Recipe{
     calculateServings(){
         this.servings = 4;
     }
+
+    parseIngridents(){
+
+        const unitsLong = ['tablespoons',"tablespoon","ounces","ounce","teaspoons","teaspoon","cups","pounds"];
+
+        const unitsShort = ['tbsp','tbsp','oz',"oz","tsp","tsp","cup","pound"];
+
+        const units = [...unitsShort,"kg","g","lbs"];
+
+        const newIngridents = this.ingredients.map(item =>{
+
+            let ingredient = item.toLowerCase();
+
+            unitsLong.map((unit,i) =>{
+                ingredient = ingredient.replace(unit,unitsShort[i]);
+            });
+
+            ingredient = ingredient.replace(/ *\([^)]*\) */g," ");
+
+            const ingredientString = ingredient.split(" ");
+
+            const IsNumber = ingredientString.findIndex(item =>units.includes(item));
+        
+            let ingredientObject;
+
+            if(IsNumber > -1) {
+
+                const ingredientNumber = ingredientString.slice(0,IsNumber);
+
+                let count;
+
+                if(ingredientNumber.length ===1){
+                    
+                    count = eval(ingredientNumber[0].replace("-","+"));
+                
+                } else {
+
+                    count = eval(ingredientNumber.slice(0,IsNumber).join("+"));
+
+                }
+
+                ingredientObject ={
+                    count,
+                    unit : ingredientString[IsNumber],
+                    ingredient : ingredientString.slice(IsNumber+1).join(" ")
+                }
+
+            } else if(parseInt(ingredientString[0],10)){
+                
+                ingredientObject ={
+                    count:  parseInt(ingredientString[0],10),
+                    unit: '',
+                    ingredient : ingredientString.slice(1).join(" ")
+                }
+
+            } else if(IsNumber === -1){
+
+                ingredientObject = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                }
+            }
+
+            return ingredientObject;
+        });
+
+        this.ingredients = newIngridents;
+
+    }
 }
